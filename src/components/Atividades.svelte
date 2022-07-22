c070a9a5c67fd880b43617148e0bd4e3eb749d76
Alinecampanholo <alinecompany@gmail.com>
2022-07-22 11:25:31 -0300

    ajustes footer

---
+++ src/components/Atividades.svelte
@@ -0,0 +1,21 @@
+<body>
+    <link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@400;500;700&display=swap" rel="stylesheet">
+    <section class="atividades">
+        <h4>ATIVDADES</h4>
+        <H2>O que rola aqui</H2>
+        <p>Em Meados de 2009 Anderson “buiu” sentiu a necessidade de retribuir ao mundo a ajuda que recebeu a decidiu fazer na frente “de casa” um cinema para as crianças da comunidade.</p>
+    </section>    
+
+</body>
+
+<style>
+    .atividades{
+        width: 1440px;
+        height: 1068px;
+        background: #000000;
+        font-family: 'Encode Sans';
+        color: #FFFFFF;
+        
+    }
+
+</style>
