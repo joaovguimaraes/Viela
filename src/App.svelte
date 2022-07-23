0110175809aeb3dd322a8f52404450e0e2793181
Alinecampanholo <alinecompany@gmail.com>
2022-07-23 09:48:11 -0300

    firt commit atividades

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script>
 	import Home from "./components/home.svelte";
+	import Atividades from "./components/atividades.svelte";
 	import Footer from "./components/footer.svelte";
+
 </script>
 
 <main>
 	<Home />
+	<Atividades/>
 	<Footer />
+
 </main>
 
 <style>
