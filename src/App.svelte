3c38a16f47f8e14dde365ff208bf71e9abe2f468
joaovguimaraes <vivito2@gmail.com>
2022-07-23 14:49:16 -0300

    blog

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,14 @@
 <script>
-	import Home from "./components/home.svelte";
-	import Atividades from "./components/atividades.svelte";
-
-	import Institucional from "./components/institucional.svelte";
-	import Footer from "./components/footer.svelte";
+	
 </script>
 
-<main>
-	<Home />
+<style>
 
-	<Atividades/>
-	<Institucional />
-	<Footer />
+</style>
+
+<main>
+
 
 </main>
 
-<style>
-	
-</style>
+
