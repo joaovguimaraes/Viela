ad60412e45e6fe42d41be695952f2463253712fe
joaovguimaraes <vivito2@gmail.com>
2022-08-02 18:13:24 -0300

    feat: :sparkles: new pages added

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,24 @@
 <script>
 	import Home from "./components/home.svelte";
 	import Atividades from "./components/atividades.svelte";
-
 	import Institucional from "./components/institucional.svelte";
 	import Footer from "./components/footer.svelte";
+	import Historias from "./components/historias.svelte";
+	import Equipe from "./components/equipe.svelte";
 </script>
 
 <main>
 	<Home />
-
-	<Atividades/>
 	<Institucional />
+	<Atividades />
+	<Historias />
+	<Equipe />
 	<Footer />
-
 </main>
 
 <style>
-	:global(body) {margin: 0; padding: 0; }
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
 </style>
---
+++ src/components/historias.svelte
@@ -0,0 +1,119 @@
+<section>
+   <div class="textos">
+      <h2>HISTÓRIAS</h2>
+      <h1>
+         Muito prazer,
+         <br /> eu sou a Yasmim
+      </h1>
+      <p>
+         Eu participo do Projeto Viela desde 2009. Organizei o
+         <br /> Cine Viela, ajudei o Futebol e Leitura, fiz aulas de inglês
+         <br /> e recentemente me apaixonei pelas aulas de gastronomia.
+         <br /> Sonho com o dia em que poderei dar aula de culinária
+         <br /> para as crianças da comunidade.
+      </p>
+      <a href="#"> Conheça outras histórias </a>
+   </div>
+   <div class="imagens_container">
+      <picture class="yasmim">
+         <img src="Yasmim.png" />
+      </picture>
+      <img src="Star_left.png" class="Star_left" alt="start"/>
+      <img src="Ellipse_roxa.png" class="Ellipse_roxa" alt="elipse"/>
+      <img src="Star_right_1.png" class="Star_right_1" alt="star"/>
+      <img src="Group_2.png" class="Group_2" alt="group 2"/>
+      <img src="Group_3.png" class="Group_3" alt="group 3"/>
+      <img src="Star_right_2.png" class="Star_right_2" alt="star"/>
+   </div>
+</section>
+
+<style>
+   @import url("http://fonts.googleapis.com/css2?family=Encode+Sans:wght@200;300;400;500;600;700&display=swap");
+
+   section {
+      font-family: "Encode Sans", sans-serif;
+      display: flex;
+      width: 100%;
+   }
+
+   .textos {
+      padding: 120px 0px 183px 142px;
+   }
+
+   .textos > h2 {
+      font-weight: 500;
+      font-size: 16px;
+      margin: 0;
+      padding-bottom: 8px;
+   }
+
+   .textos > h1 {
+      font-weight: 700;
+      font-size: 40px;
+      margin: 0;
+      padding-bottom: 17px;
+   }
+
+   .textos > p {
+      font-weight: 400;
+      font-size: 16px;
+      margin: 0;
+      padding-bottom: 48px;
+   }
+
+   .textos > a {
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 20px;
+
+      text-decoration: none;
+      color: #000000;
+      border-bottom: 1px solid #000;
+      padding-bottom: 4px;
+   }
+
+   .imagens_container {
+      position: relative;
+   }
+
+   .yasmim {
+      position: absolute;
+      left: 300px;
+   }
+
+   .Star_left {
+      position: absolute;
+      left: 119px;
+      bottom: 156px;
+   }
+
+   .Ellipse_roxa {
+      position: absolute;
+      left: 187px;
+      top: 293px;
+   }
+
+   .Star_right_1 {
+      position: absolute;
+      left: 344px;
+      top: 303px;
+   }
+
+   .Group_2 {
+      position: absolute;
+      left: 195px;
+      bottom: 50px;
+   }
+
+   .Group_3 {
+      position: absolute;
+      left: 187px;
+      bottom: 65px;
+   }
+
+   .Star_right_2 {
+      position: absolute;
+      left: 382px;
+      top: 480px;
+   }
+</style>
